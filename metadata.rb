6cf9e014fd8aa6c4aef0eba5d094d00d9fb436cb
Aaron M. Wartner <aawartner@gmail.com>
2017-12-09 23:42:07 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,12 @@ maintainer_email 'aawartner@gmail.com'
 license 'MIT'
 description 'Installs and configures BackupPC'
 long_description 'Installs and configures BackupPC'
-version '1.5.0'
+version '1.0.0'
 
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 13.0' if respond_to?(:chef_version)
+
+issues_url ''
+source_url ''
 
 depends 'apt', '~> 6.0'
 depends 'htpasswd', '~> 0.3'
