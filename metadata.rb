f5d773e37d96cd7cd8b42aec39cada29e7883f24
Aaron M. Wartner <aawartner@gmail.com>
2017-12-12 23:58:44 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aawartner@gmail.com'
 license 'MIT'
 description 'Installs and configures BackupPC'
 long_description 'Installs and configures BackupPC'
-version '1.0.2'
+version '1.0.3'
 
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
