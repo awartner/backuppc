f2b0255380a9755df6bc869b3235955d23575221
Aaron M. Wartner <aawartner@gmail.com>
2017-12-15 23:13:39 -0500

    Remove dead code

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -24,8 +24,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-Chef::Recipe.send(:include, OpenSSLCookbook::RandomPassword)
-
 include_recipe 'apt'
 
 package 'fcgiwrap'
