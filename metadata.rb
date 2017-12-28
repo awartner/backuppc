86d6fd0713357675d502acbc6d8d96472038c963
Aaron M. Wartner <aawartner@gmail.com>
2017-12-27 22:36:46 -0500

    Add a postfix email alias

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aawartner@gmail.com'
 license 'MIT'
 description 'Installs and configures BackupPC'
 long_description 'Installs and configures BackupPC'
-version '1.1.0'
+version '1.2.0'
 
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
@@ -17,4 +17,5 @@ depends 'apt', '~> 6.0'
 depends 'chef_nginx', '~> 6.0'
 depends 'htpasswd', '~> 0.3'
 depends 'openssl', '~> 7.0'
+depends 'postfix'
 depends 'sudo', '~> 3.0'
--- recipes/server.rb
+++ recipes/server.rb
@@ -24,6 +24,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+node.default['postfix']['aliases'] = { backuppc: 'root' }
+
 include_recipe 'apt'
 
 package 'backuppc'
